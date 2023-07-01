cab20fe7a3cd72c2f510cc3dc362ca83867e8cc4
zaxwebs <zifidigital@gmail.com>
2023-07-01 16:36:09 +0530

    updated styles

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,6 +4,6 @@
 @section('content')
     <h1>Simplified Logistics Management</h1>
     <p>Logistics MVC is a quick protoype app built with Leaf MVC.</p>
-    <a role="button" href="">Dashboard</a> <a role="button" class="outline" href="">GitHub
-        Repo</a>
+    <a class="mr-1" role="button" href="">Dashboard</a>
+    <a role="button" class="outline" href="">GitHub Repo</a>
 @endsection
