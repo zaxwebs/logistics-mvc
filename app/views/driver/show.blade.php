23fe772bec9ef89950d9098b789ad0f12e338da5
zaxwebs <zifidigital@gmail.com>
2023-07-04 05:45:31 +0530

    updated driver view

--- app/views/driver/show.blade.php
+++ app/views/driver/show.blade.php
@@ -4,10 +4,26 @@
     <h1>Driver Information</h1>
     @include('shared.alert')
 
-    <div class="driver-info">
-        <p><strong>Name:</strong> {{ $driver->name }}</p>
-        <p><strong>Contact Number:</strong> {{ $driver->contact_number }}</p>
-        <p><strong>License Number:</strong> {{ $driver->license_number }}</p>
-        <p><strong>Vehicle Number:</strong> {{ $driver->vehicle_number }}</p>
+    <div class="max-w-lg">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <td>{{ $driver->name }}</td>
+                </tr>
+                <tr>
+                    <th>Contact Number</th>
+                    <td>{{ $driver->contact_number }}</td>
+                </tr>
+                <tr>
+                    <th>License Number</th>
+                    <td>{{ $driver->license_number }}</td>
+                </tr>
+                <tr>
+                    <th>Vehicle Number</th>
+                    <td>{{ $driver->vehicle_number }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
